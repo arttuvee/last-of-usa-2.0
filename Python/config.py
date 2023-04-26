b59bbb094726a975caf89bd794e8f798f3bb922a
arttuvee <arttuvo@metropolia.fi>
2023-04-26 15:45:06 +0300

    Changes by aleks

--- Python/config.py
+++ Python/config.py
@@ -1,13 +1,12 @@
 # Game parameters
 max_distance = 20000
-max_lat_dist = max_distance/50
-max_lon_dist = max_distance/50
 
 water = 0
 food = 0
 medicine = 0
 solar = 0
 p_day = 1
+days_left = 10
 
 default_starting_point = "KFJK"
 default_name = "Anna"
